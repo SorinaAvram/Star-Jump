9554e42cf890daaa58ee02edce037cb52fb32c0b
Sorina <Sorina@home-8a9f3075bd>
2015-01-18 23:23:31 +0200

    Don't really know why i can't paint it on the Board

--- Star Jump/src/com/StarJump/Bricks.java
+++ Star Jump/src/com/StarJump/Bricks.java
@@ -1,20 +1,26 @@
 package com.StarJump;
 
 import java.awt.*;
-
 import javax.swing.*;
 
 public class Bricks   {
 	
 	Image imgb;
 	int x,  y;
-	boolean full = true;
-	
+	boolean breaks;	
 	ImageIcon l = new ImageIcon("D://Poze/Bricks04.png");
-	public  Bricks() {
+	public  Bricks(int x, int y) {
 		
-	    x = 250;
-	    y = 220;		
-		imgb = l.getImage();	
+		this.x = x;
+	    this.y = y;		
+		imgb = l.getImage();
+	breaks = false;
+	   
+	}	
+	public boolean notBreaks() {
+		return breaks;
+	}
+	public void setBreaks (boolean breaks){
+		this.breaks =breaks;
 	}
 }
