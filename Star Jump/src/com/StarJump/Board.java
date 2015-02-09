f6341a9fc8addd0f707f08d27807d5b1c3b93109
Sorina <Sorina@home-8a9f3075bd>
2015-02-09 20:46:20 +0200

    dude.y = val

--- Star Jump/src/com/StarJump/Board.java
+++ Star Jump/src/com/StarJump/Board.java
@@ -5,148 +5,146 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
-public class Board extends JPanel implements ActionListener,Runnable{
+public class Board extends JPanel implements ActionListener, Runnable {
 
-	
-	
-/**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-Character dude;
-Image img ;
-Timer time;
-Rectangle floor; 
-int val = 290;
-Thread animator;
-Bricks b1;
-
+	Character dude;
+	Image img;
+	Timer time;
+	Rectangle floor;
+	int val = 290;
+	Thread animator;
+	Bricks b1;
+	boolean lost = false;
 
 	public Board() {
 		dude = new Character();
-		b1 = new Bricks(180,290);	
+		b1 = new Bricks(180, 290);
 		addKeyListener(new AL());
 		setFocusable(true);
-		ImageIcon i = new ImageIcon("backround.png");		
-		img = i.getImage();		
-		
+		ImageIcon i = new ImageIcon("backround.png");
+		img = i.getImage();
+
 		time = new Timer(5, this);
 		time.start();
-		
-				
+
 		defineObjects();
-		 	}
-	public Color floorColor = new Color(122,52,52);
- 	
-	public int floorheight= 60;
+	}
+
+	public Color floorColor = new Color(122, 52, 52);
+
+	public int floorheight = 60;
 	public int fps = 60;
 	public boolean objectDefine = false;
 	public Thread game;
-	public Board(Display f){			
-	
 
-				
-		defineObjects();		
+	public Board(Display f) {
+
+		defineObjects();
 		game.start();
-		}
-		
-
-		
-		
-	void defineObjects() { 
-				floor = new Rectangle (0, FirstImportantClass.height - floorheight, FirstImportantClass.width, floorheight);
-				
-		 		
-		 		objectDefine = true;
-		 		repaint();		 
-		 						
-}
-	
-	
-	boolean k=false;
-	public void paint(Graphics g) {
-				if (dude.dy ==1 && k==false){
-					k=true;
-					animator = new Thread(this);
-					animator.start();
-				}
-		
-		 		super.paint(g);			
-		 		
-		 		if(objectDefine) {			
-		 		g.setColor(floorColor);
-		 		g.fillRect(floor.x, floor.y, floor.width,floor.height);
-		 		g.drawImage(img, 0, 0, this);
-		 		g.drawImage(dude.getImage(), dude.getX(), val, this);
-		 		g.drawImage(b1.getImage(), b1.getX(), b1.getY(), this);
-		 			}	
-}		
-
-		 			
-	public void actionPerformed(ActionEvent e) {				
-				dude.move();
-				checkCollision();
-		 		repaint();
-	 }
-	
-	public void checkCollision() {	
-		
-	Rectangle r1 = b1.getBounds();
-	Rectangle r2 = dude.getBounds();
-	
-	if (r2.intersects(r1) && b1.notBreaks()){
-		b1.breaks = false ;	
-		done = true;
 	}
-		
 
-	
-	}	
-	
-	
+	void defineObjects() {
+		floor = new Rectangle(0, FirstImportantClass.height - floorheight,
+				FirstImportantClass.width, floorheight);
+
+		objectDefine = true;
+		repaint();
+
+	}
+
+	boolean k = false;
+
+	public void paint(Graphics g) {
+		if (lost)
+			System.exit(0);
+		if (dude.dy == 1 && k == false) {
+			k = true;
+			animator = new Thread(this);
+			animator.start();
+		}
+
+		super.paint(g);
+
+		if (objectDefine) {
+			g.setColor(floorColor);
+			g.fillRect(floor.x, floor.y, floor.width, floor.height);
+			g.drawImage(img, 0, 0, this);
+			g.drawImage(dude.getImage(), dude.getX(), val, this);
+			g.drawImage(b1.getImage(), b1.getX(), b1.getY(), this);
+		}
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		dude.move();
+		repaint();
+	}
+
+	public void checkCollision() {
+
+		Rectangle r1 = b1.getBounds();
+		Rectangle r2 = dude.getBounds();
+
+		if (r2.intersects(r1) && b1.notBreaks()) {
+			b1.breaks = false;
+
+		}
+
+	}
+
 	private class AL extends KeyAdapter {
-		public void keyReleased (KeyEvent e){	
+		public void keyReleased(KeyEvent e) {
 			dude.keyReleased(e);
 		}
-		public void keyPressed (KeyEvent e){
+
+		public void keyPressed(KeyEvent e) {
 			dude.keyPressed(e);
 		}
 	}
+
 	@Override
-	public void run() {		
+	public void run() {
 		long beforeTime, timeDiff, sleep;
-		
+
 		beforeTime = System.currentTimeMillis();
-		while(done==false) {
+		while (done == false) {
 			cycle();
-			timeDiff= System.currentTimeMillis() - beforeTime;
+			checkCollision();
+			timeDiff = System.currentTimeMillis() - beforeTime;
 			sleep = 10 - timeDiff;
 			if (sleep < 0)
-				sleep=2;
+				sleep = 2;
 			try {
 				Thread.sleep(sleep);
-				
-			}catch(Exception e){
+
+			} catch (Exception e) {
+			}
+			beforeTime = System.currentTimeMillis();
 		}
-			beforeTime=System.currentTimeMillis();
-	}
 		done = false;
 		peak = false;
-		k=false;
-		
+		k = false;
+
 	}
+
 	boolean peak = false;
 	boolean done = false;
-	public void cycle() {
-		
-		if (peak == false)
-			val --; 
-		if(val ==210)
-			peak=true;
-		if(peak == true && val <=290)
-			val++;
-		if (val==290)
-			done=true;
-}
 	
+	public void cycle() {
+
+		if (peak == false)
+			val--;
+			dude.y=val;
+		if (val == 210)
+			peak = true;
+		if (peak == true && val <= 290)
+			val++;
+			dude.y=val;
+		if (val == 290)
+			done = true;
+	}
+
 }
--- Star Jump/src/com/StarJump/Character.java
+++ Star Jump/src/com/StarJump/Character.java
@@ -6,76 +6,83 @@ import java.awt.event.KeyEvent;
 import javax.swing.*;
 
 public class Character {
-	
-int x, dy, y, dx;
-Image still;
-int width = 74;
-int height = 72;
-int halfW, halfH;
-Rectangle bounds;
 
-ImageIcon i1 = new ImageIcon("25.png");
-ImageIcon j = new ImageIcon("jump.png");
+	int x, dy, y, dx;
+	Image still;
+	int width = 74;
+	int height = 72;
+	int halfW, halfH;
+	Rectangle bounds;
 
-	public Character (){
-	
-	still = i1.getImage();	
-	x = 260;
-    y = 290;
-    
-    halfW = width / 2;
-	halfH = height / 2;
-	
-	bounds = new Rectangle(width, height);
-	
-}
+	ImageIcon i1 = new ImageIcon("25.png");
+	ImageIcon j = new ImageIcon("jump.png");
+
+	public Character() {
+
+		still = i1.getImage();
+		x = 260;
+		y = 290;
+
+		halfW = width / 2;
+		halfH = height / 2;
+
+		bounds = new Rectangle(width, height);
+
+	}
+
 	public void move() {
-		x = x + dx ;
-}
-	public int getX(){
+		x = x + dx;
+	}
+
+	public int getX() {
 		return x;
 	}
-	public int getY(){
+
+	public int getY() {
 		return y;
-}
-	public Image getImage(){
+	}
+
+	public Image getImage() {
 		return still;
-}
-	public void keyPressed(KeyEvent e){
+	}
+
+	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
-		if (key == KeyEvent.VK_LEFT){
-		dx = -1;
-		still = i1.getImage();
-	}
-		if (key == KeyEvent.VK_RIGHT){
-		dx = 1;
-		still = i1.getImage();
-	}
-		
-		if(key == KeyEvent.VK_UP){
-			dy =1;
-		still = j.getImage();
+		if (key == KeyEvent.VK_LEFT) {
+			dx = -1;
+			still = i1.getImage();
+		}
+		if (key == KeyEvent.VK_RIGHT) {
+			dx = 1;
+			still = i1.getImage();
+		}
+
+		if (key == KeyEvent.VK_UP) {
+			dy = 1;
+			still = j.getImage();
 		}
 	}
+
 	public void keyReleased(KeyEvent e) {
 		int key = e.getKeyCode();
-		if (key == KeyEvent.VK_LEFT){
-		dx = 0;
-		
+		if (key == KeyEvent.VK_LEFT) {
+			dx = 0;
+
 		}
-		if (key == KeyEvent.VK_RIGHT){
-		dx = 0;
+		if (key == KeyEvent.VK_RIGHT) {
+			dx = 0;
 		}
-		if(key == KeyEvent.VK_UP){
-		dy =0;
-		still = i1.getImage();
-		
+		if (key == KeyEvent.VK_UP) {
+			dy = 0;
+			still = i1.getImage();
+
 		}
-		
+
 	}
+
 	public Rectangle getBounds() {
-	bounds.setLocation(x - halfW, y + halfH);		
+		bounds.setLocation(x - halfW, y + halfH);
 		return bounds;
 	}
-		
+
 }
